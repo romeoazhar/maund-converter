ccebb97acf250eb56ea22a9de76dd5d9a1603bdb
romeoazhar <132354515+romeoazhar@users.noreply.github.com>
2026-09-12 02:43:14 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'maund-app-v1';
+const FILES_TO_CACHE = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
